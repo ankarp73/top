654a37fd6435b57482b7b4ab2730f2908049f76a
MKA <MKA@ULTOP.local>
2023-05-16 19:34:14 +0400

    16.05.23 TOP в классе 19-35

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 print(2+5)
+
+# a=2 # int
+# print(a, type(a)) # int
+
+# a=str(a) #str
+# print(a, type(a))
+
+# z=input("Введите число - мы умножим на 2: ")  # str
+# z=int(z) # из строки сделали число
+# print(z*2)
+
+print("Введите информацию о себе")
+my_name=input("Ввелите имя: ")
+
+print("Информация о пользователе")
+print("Имя:", my_name)
+
+my_age=input("Введите ваш возраст: ")
+print("My_age:", my_age)
+
+my_number=input("Введите ваш номер сотового телефона: ")
+print('my_number:', my_number)
+
+my_height=input("Ваш рост: ")
+weight=input("Ваш вес: ")
+
+print("height:", my_height)
+print("weight:", weight)
