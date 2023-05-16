f08b3c988c751594164d67a6f1eef66217a85060
MKA <MKA@ULTOP.local>
2023-05-16 21:21:12 +0400

    top 16/05 21:20

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(2+5)
+# print(2+5)
 
 # a=2 # int
 # print(a, type(a)) # int
@@ -10,20 +10,48 @@ print(2+5)
 # z=int(z) # из строки сделали число
 # print(z*2)
 
-print("Введите информацию о себе")
-my_name=input("Ввелите имя: ")
+# print("Введите информацию о себе")
+# my_name=input("Ввелите имя: ")
 
-print("Информация о пользователе")
-print("Имя:", my_name)
+# print("Информация о пользователе")
+# print("Имя:", my_name)
 
-my_age=input("Введите ваш возраст: ")
-print("My_age:", my_age)
+# my_age=input("Введите ваш возраст: ")
+# print("My_age:", my_age)
 
-my_number=input("Введите ваш номер сотового телефона: ")
-print('my_number:', my_number)
+# my_number=input("Введите ваш номер сотового телефона: ")
+# print('my_number:', my_number)
 
-my_height=input("Ваш рост: ")
-weight=input("Ваш вес: ")
+# my_height=input("Ваш рост: ")
+# weight=input("Ваш вес: ")
 
-print("height:", my_height)
-print("weight:", weight)
+# print("height:", my_height)
+# print("weight:", weight)
+
+# print("привет" + " " + myName)
+# print(f"привет {2+2}")
+# print("привет"," ","мир!")
+
+
+# x = int(input("введите число: ")) #26
+# print(x/2)
+
+# # используем // и %
+# print(x//10)
+# print(x%10)
+# a = 2
+# b = 2
+# h = a / b * 10
+
+# print(int(h))
+# print(type(h))
+
+x = 891 
+a = x
+a1 = x // 100
+print(a1)
+a2 = x // 10 % 10
+print(a2)
+a3 = x % 10
+print(a3)
+print(a1+a2+a3)
