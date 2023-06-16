54383f6a1ab90e85c0a2e1143a8be2a6a4915921
MKA <MKA@ULTOP.local>
2023-06-16 21:27:05 +0400

    Функция -regRace

--- TOP/main3.py
+++ TOP/main3.py
@@ -83,36 +83,107 @@
 
 # _______________________________________________________________________________
 
-guestList = []
-while True:
-    nameGuest = input("Введите имя гостя: ")
-    ageGuest = int(input("Введите возраст гостя :"))
-    # Выше созданные переменные будут появляться в объект infoGuest
-    # и вставляться в сооттветствующие ключи
-    # infoGuest хранит данные в гостя
-    infoGuest = {
-        "nameGuest" : nameGuest,
-        "ageGuest" : ageGuest,
-    }
-    # print(infoGuest)
-    guestList.append(infoGuest)
-    if len (guestList) > 3:
-        break
+# guestList = []
+# while True:
+#     nameGuest = input("Введите имя гостя: ")
+#     ageGuest = int(input("Введите возраст гостя :"))
+#     # Выше созданные переменные будут появляться в объект infoGuest
+#     # и вставляться в сооттветствующие ключи
+#     # infoGuest хранит данные в гостя
+#     infoGuest = {
+#         "nameGuest" : nameGuest,
+#         "ageGuest" : ageGuest,
+#     }
+#     # print(infoGuest)
+#     guestList.append(infoGuest)
+#     if len (guestList) > 3:
+#         break
 
-# print(guestList)
+# # print(guestList)
 
-for i in range(0,len(guestList)):
-    print(f"Имя гостя - {guestList[i]['nameGuest']}")
-    print(f"Возраст гостя - {guestList[i]['ageGuest']}")
-    print("---------")
+# for i in range(0,len(guestList)):
+#     print(f"Имя гостя - {guestList[i]['nameGuest']}")
+#     print(f"Возраст гостя - {guestList[i]['ageGuest']}")
+#     print("---------")
 
-# ________________________________________________________________________________
-
-
+# # ________________________________________________________________________________
 
 
 
-#  13 июня   Вторник  
+
+
+#  13 июня   Вторник  Была контрольная 5 заданий
 #  
-#    
+#  ------------------------------------------------------------------------------
+# 16 июня   Пятница Знакомство с функциями
 
+# numEnt = int(input("загадайте число\n от 0 до 100: "))
+# print("Каждая итерация будет ровно делиться частями,\nт.е. с каждой последующей итерацией диапазон будет делиться пополам.")
+
+# -----------------------------------------
+
+
+# def reg(x):
+
+#     print(x)
+
+# reg(input("Введите своё имя: "))
+
+#  ------------------------------------------------------------------------
+
+# def regName(myName):
+#     print(myName)
+
+# def regGender():
+#     listGender = ["муж","жен"]
+#     textGender = ""
+#     for i in range(0,len(listGender)):
+#         textGender += f"{i} - {listGender[i]}\n"
+#     # print(textGender)
+#     myGender = int(input(f"{textGender}\n:"))
+#     for i in range(0,len(listGender)):
+#         if myGender == i:
+#             myGender = listGender[i]
+#             break
+
+# def globalReg():
+#     print("Рег персонажа")
+#     x = int(input("Выбор действия: \n1-Ввод имени>\n2-Выбор Гендера>\n:"))
+#     if x == 1:
+#         regName(input("Имя: "))
+#     elif x == 2:
+#         regGender()
+
+# globalReg()
+
+# def regRace():
+#     pass
+#  --------------------------------------------------------------------------
+
+
+def regName(myName):
+    print(myName)
+    
+def regRace():
+    listRace = ['Человек','Эльф','Гном','Орк','Троль','Гоблин',]
+    textRace = ""
+    for i in range(0,len(listRace)):
+        textRace += f"{i} - {listRace[i]}\n"
+    # print(textRace)
+    myRace = int(input(f"{textRace}\n:"))
+    for i in range(0,len(listRace)):
+        if myRace == i:
+            myRace = listRace[i]
+            break
+    print(f"Ты выбрал: {myRace}")
+    # globalReg()
+
+def globalReg():
+    print("Рег персонажа")
+    x = int(input("Выбор действия: \n1-Ввод имени>\n2-Выбор Race>\n:"))
+    if x == 1:
+        regName(input("Имя: "))
+    elif x == 2:
+        regRace()
+
+globalReg()
