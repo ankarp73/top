cbd473955f2848c3ca2de8463f50aa4be92d40ef
MKA <MKA@ULTOP.local>
2023-06-29 21:11:33 +0400

    Четв 29.06.23 Class

--- TOP/main4.py
+++ TOP/main4.py
@@ -89,46 +89,164 @@
 
 # _____________________________________________________
 
-import copy
+# import copy
 
-def attack():
-    print("Включен режим атаки")
+# def attack():
+#     print("Включен режим атаки")
 
-Person = {
-    "name" : "Варвар",
-    "gender" : "Мужской",
-    "actions" : {
-        "attack" : attack
-    }
-}
-
-
-Human = copy.deepcopy(Person)
-Human["race"] = "Человек"
-Human["skills"] = ["Быстрый бег", "Красноречие"]
-
-Warrion = copy.deepcopy(Human)
-Warrion["role"] = "Воин"
-Warrion["desk"] = ["Воин отличается своим сочетанием мобильности, живучести,способности..."]
-
-Archer = copy.deepcopy(Human)
-Archer["role"] = "Лучник"
-Archer["desk"] = ["Лучники способны избегать все эффекты контроля, получать меньше входящего урона..."]
-Archer["action"]["attack"] = shoot()
+# Person = {
+#     "name" : "Варвар",
+#     "gender" : "Мужской",
+#     "actions" : {
+#         "attack" : attack
+#     }
+# }
 
 
-Orc = copy.deepcopy(Person)
-Orc["race"] = "Орк"
-Orc["skills"] = ["Сила", "Быстрый бег"]
+# Human = copy.deepcopy(Person)
+# Human["race"] = "Человек"
+# Human["skills"] = ["Быстрый бег", "Красноречие"]
 
-print("-------")
-print(Person)
-print("-------")
-print(Human)
-print("-------")
-print(Orc)
-print("-------")
-print(Warrion)
-print("-------")
-print(Archer)
-print("-------")
+# Warrion = copy.deepcopy(Human)
+# Warrion["role"] = "Воин"
+# Warrion["desk"] = ["Воин отличается своим сочетанием мобильности, живучести,способности..."]
+
+# Archer = copy.deepcopy(Human)
+# Archer["role"] = "Лучник"
+# Archer["desk"] = ["Лучники способны избегать все эффекты контроля, получать меньше входящего урона..."]
+# Archer["action"]["attack"] = shoot()
+
+
+# Orc = copy.deepcopy(Person)
+# Orc["race"] = "Орк"
+# Orc["skills"] = ["Сила", "Быстрый бег"]
+
+# print("-------")
+# print(Person)
+# print("-------")
+# print(Human)
+# print("-------")
+# print(Orc)
+# print("-------")
+# print(Warrion)
+# print("-------")
+# print(Archer)
+# print("-------")
+
+# *********************************************************************************************************
+# _______________________________________ Четверг 29.06.2023 ______________________________________________
+# _____________________________________________ class _____________________________________________________
+# *********************************************************************************************************
+
+
+# class Car:
+#     def __init__(self, color, marka, motor):
+#         self.color = color
+#         self.marka = marka
+#         self.motor = motor
+
+
+#     # методы - действия с определённым классом
+#     def showColor(self):
+#         print(self.color)
+
+#     def showMarka(self):
+#         print(self.marka)
+
+
+# class Engine:
+#     def __init__(self, HP, volume):
+#         self.HP = HP
+#         self.volume = volume
+
+#     def start(self):
+#         print("Запуск")
+
+#     def stop(self):
+#         print("Стоп")
+
+
+# class Conditioner:
+#     def __Conditioner__(self, cooling):
+#         self.cooling = cooling
+
+
+# myEngine = Engine(120, 2)
+# twoEngine = Engine(280,2.2)
+# myAuto = Car("green", "audi", myEngine)
+
+# myAuto.motor.start()
+
+
+# class Wheel:
+#     def __init__(self, marka, seasonality, radius, profile):
+#         self.marka = marka
+#         self.seasonality =seasonality
+#         self.radius =radius
+#         self.profile =profile
+
+#     def turn_left(self):
+#         print("Повернуть налево")
+
+#     def turn_right(self):
+#         print("Повернуть на право")
+
+#     def take_off(self):
+#         print("Снять колесо")
+
+#     def put_on(self):
+#         print("Надеть колесо")
+
+
+# # class Light:
+# #     def __init__(self, type, voltage, plinth)
+
+# print(myAuto.motor.volume)
+
+
+
+#  # Наследование
+# # class EngineSport()
+
+# twoEngine = Engine(500,2)
+
+# class SportCar(Car):
+#     def __init__(self, color, marka, motor,turbo):
+#         self.turbo = turbo
+#         super().__init__(color, marka, motor)
+
+# lamborginiEngine = Engine(900, 6)
+# twoAuto = SportCar("blue","lamborgini", twoEngine, True)
+
+# twoAuto.showMarka()
+
+
+
+
+
+
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def activeSound(self):
+        print(self.sound)
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Мяу")
+
+    def purr(self):
+        print("Мурлыкает")            
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Гав")
+
+    def digHole(self):
+        print("Копает яму")
+
+myCat = Cat("Вася")
+myCat.purr()
+
