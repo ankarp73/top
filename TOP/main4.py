48475b999eefbbc3754801b58e057c4e4422b570
MKA <MKA@ULTOP.local>
2023-07-11 21:28:41 +0400

    вторник 11.07.23

--- TOP/main4.py
+++ TOP/main4.py
@@ -253,40 +253,280 @@
 
 # 4 июля урок (абстрактный метод)
 
-class Bird():
-    def __init__(self, name, sound):
-        self.name = name
-        self.sound = sound
+# class Bird():
+#     def __init__(self, name, sound):
+#         self.name = name
+#         self.sound = sound
 
-    def eat(self):
-        print("Ест")
+#     def eat(self):
+#         print("Ест")
 
-    def hunting(self):
-        print("Охотятся")
+#     def hunting(self):
+#         print("Охотятся")
 
-    def activeSound(self):
-        print(self.sound)
+#     def activeSound(self):
+#         print(self.sound)
 
-class noFly(Bird):
-    def __init__(self, name, sound=""):
-        super().__init__(name, sound)
+# class noFly(Bird):
+#     def __init__(self, name, sound=""):
+#         super().__init__(name, sound)
     
-    def goes(self):
-        print("Летает")
+#     def goes(self):
+#         print("Летает")
 
-class Fly(noFly):
-    def __init__(self, name, sound=""):
-        super().__init__(name, sound)
+# class Fly(noFly):
+#     def __init__(self, name, sound=""):
+#         super().__init__(name, sound)
 
-    def noFly(self):
-        print("Летает")
+#     def noFly(self):
+#         print("Летает")
 
-class Crow(Fly):
-    def __init__(self, name, sound=""):
-        super().__init__(name, "-Кар-кар!!!")
+# class Crow(Fly):
+#     def __init__(self, name, sound=""):
+#         super().__init__(name, "-Кар-кар!!!")
 
-crow = Crow("Гриша")
-crow.activeSound()
+# crow = Crow("Гриша")
+# crow.activeSound()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 21 - 07 августа каникулы
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~    Tuestoday~Вторник 11.07.2023  ~~~~~~~~~~~~~~~~~~~~~~~
+
+# хранит просто инф
+base_list = [
+        {
+        "first_name" : "Денис",
+        "last_name" : "Кириллов",
+        "birthday" : "01.06.2001",
+        "gender" : "мужской",
+        "login" : "denis161",
+        "password" : "12345",
+    },
+        {
+        "first_name" : "Антон",
+        "last_name" : "Карпов",
+        "birthday" : "02.07.2011",
+        "gender" : "мужской",
+        "login" : "anton162",
+        "password" : "12346",
+    },
+        {
+        "first_name" : "Анастасия",
+        "last_name" : "Капустина",
+        "birthday" : "03.08.2012",
+        "gender" : "женский",
+        "login" : "настя163",
+        "password" : "12347",
+    },
+        {
+        "first_name" : "Вераника",
+        "last_name" : "Мастина",
+        "birthday" : "04.09.2013",
+        "gender" : "женский",
+        "login" : "верон164",
+        "password" : "12348",
+    },
+        {
+        "first_name" : "Вася",
+        "last_name" : "Пупкин",
+        "birthday" : "11.09.2015",
+        "gender" : "мужской",
+        "login" : "вася165",
+        "password" : "12348",
+    },
+]
+
+register_users = [
+
+
+]
+
+class User():
+    def __init__(self, user_id, first_name, last_name, birthday, gender, login,
+                password):
+                self.user_id = user_id
+                self.first_name = first_name
+                self.last_name = last_name
+                self.birthday = birthday
+                self.gender = gender
+                self.login = login
+                self.password = password
+                # ~~~~~~~~~~~~~~~~~~~~~~~
+                self.status = "user"
+                self.blocking = False
+
+    # Если в классе есть методы с словом update,значит, этот метод для изм инф
+
+    def update_first_name(self, new_first_name):
+        self.first_name = new_first_name
+
+    def update_last_name(self, new_last_name):
+        self.last_name = new_last_name
+
+    def update_birthday(self, new_birthday):
+        self.birthday = new_birthday
+
+    def update_gender(self, new_gender):
+        self.gender = new_gender
+
+    def update_password(self, new_password):
+        if self.password == input("Введите старый пароль:  "):
+            self.password = new_password
+
+class Moderator(User):
+    def __init__(self, user_id, first_name, last_name, birthday, gender, login,
+    password):
+        self.status = "moderator"
+        super().__init__(user_id, first_name, last_name, birthday, gender, login,
+        password )
+
+    def blocking_user(self, users_list):
+        """для блокировки пользователя"""
+        text_user_list = f"id | first_name | blocking | status \n"
+        for i in range(0, len(users_list)):
+            text_user_list += f"{i} - {users_list[i].user_id}{users_list[i]['first_name']} - {users_list[i]['blocking']} {users_list[i]['status']}\n"
+        print(text_user_list)
+        input_user_id = int(input("Введите id пользователя для блокировки: "))
+        for i in range(0,len(users_list)):
+            if self.status == "moderator":
+                if input_user_id == i and users_list[i]['status'] != "moderator" and users_list[i]['status']!= "admin":
+                    if users_list[i]['blocking'] == True:
+                        print("пользователь уже заблоктрован")
+                        break
+                    else:
+                        users_list[i]['blocking'] = True
+                        print("пользователь успешно заблокирован")
+                        break
+
+class Admin(Moderator):
+    def __init__(self, user_id, first_name, last_name, birthday, gender, login, password):
+        super().__init__(user_id, first_name, last_name, birthday, gender, login, password)
+        self.status = "admin"
+    def delete_user_list(self, user_list):
+        user_list.clear()
+        print("База данных удалена")
+                            # massiv - явл массивом данных
+    def create_user_list(self, massiv, user_list):
+        for i in range(len(user_list),len(massiv)):
+            user_list.append(User(user_id=i,
+                                first_name=massiv[i]["first_name"],
+                                last_name=massiv[i]["last_name"],
+                                birthday=massiv[i]["birthday"],
+                                gender=massiv[i]["gender"],
+                                login=massiv[i]["login"],
+                                password=massiv[i]["password"]))
+
+class Registration():
+    def __init__(self) -> None:
+        pass
+    def create_user(self, users_list):
+        users_list.append(User(len(users_list),
+                                input("введите имя: "),
+                                input("введите фамилию: "),
+                                input("введите дату рожд: "),
+                                input("введите пол: "),
+                                input("введите логин: "),
+                                input("введите пароль: ")))
+
+class InLog():
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+    def log_in_account(self, users_list):
+        for i in range(len(users_list)):
+            if users_list[i].login == self.login and users_list[i].password == self.password:
+                print("вход выполнен")
+                break
+            else:
+                print("Ошибка")
+
+class Manager():
+    def __init__(self, reg, inlog, userModerAdmin, datalist):
+        self.reg = reg
+        self.userModerAdmin = userModerAdmin
+        self.datalist = datalist
+    def work(self):
+        print(self)
+
+
+my_reg = Registration
+my_inlog = InLog
+my_person_list = [User(),Moderator(),Admin()]        
+my_base = []
+myManager = Manager(my_reg, my_inlog, my_person_list, my_base)
+myManager.userModerAdmin[0](10,"admin","admin","01.01.01","муж","admin","admin")
+myManager.inlog("admin","admin")
+
+
+
+
+
+
+
+
+# class Manager(Registration,InLog):
+#     def __init__(self) :
+#         super().__init__()
+
+
+
+
+
+# proverka = Registration()
+# proverka.create_user()
+                                                  
+# myAdmin = Admin(10,"admin","admin","01.01.1970","Мужской","admin","admin")
+# myAdmin.create_user_list(base_list, register_users)
+
+# myLogin = InLog(input("Введ логин: "), input("Введ пароль: "))
+# myLogin.log_in_account()
+
+
+
+
+
+
+# print(register_users[0])
+# print(register_users[1].first_name)
+# register_users[0].update_first_name("Danila")
+# print(register_users[0].first_name)
+
+# continie
+# primer = [
+#     User(0,"name","last.name","01.06.2001","Мужской","login","pass"),
+#     User(0,"name","last.name","01.06.2001","Мужской","login","pass"),
+# ]
+
+
+
+
+
+
 
 
 
